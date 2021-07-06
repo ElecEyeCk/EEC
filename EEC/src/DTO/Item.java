36f5cc5d1f6425a02304e986c5d0f3ebe8636848
youqixin123 <54894668+youqixin123@users.noreply.github.com>
2021-07-06 23:43:24 +0800

    Update Item.java

--- EEC/src/DTO/Item.java
+++ EEC/src/DTO/Item.java
@@ -1,5 +1,10 @@
 package DTO;
 
+import DAO.DAO;
+import com.kitfox.svg.A;
+
+import java.util.ArrayList;
+
 public class Item {
     private String name;
     private String link;
@@ -8,8 +13,9 @@ public class Item {
     private int commit;
     private double price;
     private String shop;
+    private String date;
 
-    public Item(String name, String link, String picLink, String icons, int commit, double price, String shop) {
+    public Item(String name, String link, String picLink, String icons, int commit, double price, String shop,String date) {
         this.name = name;
         this.link = link;
         this.picLink = picLink;
@@ -17,6 +23,7 @@ public class Item {
         this.commit = commit;
         this.price = price;
         this.shop = shop;
+        this.date=date;
     }
 
     public String getLink() {
@@ -45,5 +52,39 @@ public class Item {
 
     public String getName() {
         return name;
+    }
+    
+    public String getDate() {
+        return date;
+    }
+    
+    public  Item getItem(String name){
+        ArrayList<String> columnLabels = new ArrayList<>();
+
+        columnLabels.add("name");
+        columnLabels.add("link");
+        columnLabels.add("picLink");
+        columnLabels.add("icons");
+        columnLabels.add("commit");
+        columnLabels.add("price");
+        columnLabels.add("shop");
+        columnLabels.add("date");
+        ArrayList<ArrayList<Object>>  itemObjs = DAO.search("SELECT name,link,picLink,icons,commit,price,shop,date FROM user where name='" + name + "'", columnLabels);
+        return new Item((String)itemObjs.get(0).get(0),(String)itemObjs.get(1).get(0),(String)itemObjs.get(2).get(0),(String)itemObjs.get(3).get(0),(Integer)itemObjs.get(4).get(0),(Double) itemObjs.get(5).get(0),(String)itemObjs.get(6).get(0),(String)itemObjs.get(7).get(0));
+   }
+    
+     public ArrayList<ArrayList<Object>> getItem2(String keyword){
+        ArrayList<String> columnLabels1 = new ArrayList<>();
+
+        columnLabels1.add("name");
+        columnLabels1.add("link");
+        columnLabels1.add("picLink");
+        columnLabels1.add("icons");
+        columnLabels1.add("commit");
+        columnLabels1.add("price");
+        columnLabels1.add("shop");
+        columnLabels1.add("date");
+        return DAO.search("SELECT name,link,picLink,icons,commit,price,shop,date FROM user where name LIKE'%" + keyword + "%'", columnLabels1);
+
     }
 }
